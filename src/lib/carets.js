0dcb72c6f52318dae49e36c861f951995729ba88
scratchyone <scratchywon@gmail.com>
2021-08-23 19:51:20 -0400

    Some functionality

---
+++ src/lib/carets.js
@@ -0,0 +1,50 @@
+import { browser } from '$app/env';
+
+export function getCaretPosition(el) {
+	var caretOffset = 0,
+		sel;
+	if (
+		browser &&
+		typeof window.getSelection !== 'undefined' &&
+		window.getSelection().rangeCount > 0
+	) {
+		var range = window.getSelection().getRangeAt(0);
+		var selected = range.toString().length;
+		var preCaretRange = range.cloneRange();
+		preCaretRange.selectNodeContents(el);
+		preCaretRange.setEnd(range.endContainer, range.endOffset);
+		caretOffset = preCaretRange.toString().length - selected;
+	}
+	return caretOffset;
+}
+function getAllTextnodes(el) {
+	var n,
+		a = [],
+		walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+	while ((n = walk.nextNode())) a.push(n);
+	return a;
+}
+export function getCaretData(el, position) {
+	var node;
+	let nodes = getAllTextnodes(el);
+	for (var n = 0; n < nodes.length; n++) {
+		if (position > nodes[n].nodeValue.length && nodes[n + 1]) {
+			// remove amount from the position, go to next node
+			position -= nodes[n].nodeValue.length;
+		} else {
+			node = nodes[n];
+			break;
+		}
+	}
+	// you'll need the node and the position (offset) to set the caret
+	return { node: node, position: position };
+}
+// setting the caret with this info  is also standard
+export function setCaretPosition(d) {
+	var sel = window.getSelection(),
+		range = document.createRange();
+	range.setStart(d.node, d.position);
+	range.collapse(true);
+	sel.removeAllRanges();
+	sel.addRange(range);
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { browser } from '$app/env';
+	import { tick } from 'svelte';
+	import { getCaretPosition, getCaretData, setCaretPosition } from '$lib/carets';
+	let html = '<span class="placeholder">Start typing...</span>';
+	let textarea;
+	let text = '';
+	async function keypress(e) {
+		await tick();
+		text = textarea.innerText;
+		let pos = getCaretPosition(textarea);
+		console.log(text);
+		html = text;
+		await tick();
+		console.log(pos);
+		let data = getCaretData(textarea, pos + 1);
+		setCaretPosition(data);
+	}
+</script>
+
+<pre
+	id="textarea"
+	contenteditable="true"
+	on:keypress={keypress}
+	bind:this={textarea}
+	bind:innerHTML={html}
+/>
+
+<style>
+	#textarea {
+		min-width: 100vw;
+		min-height: 100vh;
+		margin: 0px;
+		padding: 20px;
+		outline: none;
+		font-size: 17px;
+		font-family: 'system-ui';
+	}
+</style>
