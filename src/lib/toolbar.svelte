9065c94edaf615c90a385764019bb24dc04513a2
scratchyone <scratchywon@gmail.com>
2021-08-23 20:59:18 -0400

    Works

---
+++ src/lib/toolbar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faDownload } from '@fortawesome/free-solid-svg-icons';
+	import { data } from '$lib/store';
+</script>
+
+<div class="wrapper"><div class="button"><Fa icon={faDownload} /></div></div>
+
+<style>
+	.wrapper {
+		display: flex;
+		gap: 10px;
+		position: absolute;
+		right: 10px;
+		top: 10px;
+		color: #505050;
+	}
+	.button {
+		cursor: pointer;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,35 @@
 <script>
 	import { browser } from '$app/env';
-	import { tick } from 'svelte';
-	import { getCaretPosition, getCaretData, setCaretPosition } from '$lib/carets';
-	let html = '<span class="placeholder">Start typing...</span>';
+	import debounce from 'debounce';
+	import { data } from '$lib/store';
+	import Toolbar from '$lib/toolbar.svelte';
 	let textarea;
-	let text = '';
-	async function keypress(e) {
-		await tick();
-		text = textarea.innerText;
-		let pos = getCaretPosition(textarea);
-		console.log(text);
-		html = text;
-		await tick();
-		console.log(pos);
-		let data = getCaretData(textarea, pos + 1);
-		setCaretPosition(data);
+	$: {
+		if ($data == '' && textarea) textarea.placeholder = 'Start typing...';
+		else if (textarea) textarea.placeholder = '';
 	}
+	const debounced = debounce((text) => {
+		if (browser) localStorage.setItem('text', text);
+	}, 500);
+	data.subscribe(debounced);
 </script>
 
-<pre
-	id="textarea"
-	contenteditable="true"
-	on:keypress={keypress}
-	bind:this={textarea}
-	bind:innerHTML={html}
+<svelte:window
+	on:beforeunload={() => {
+		debounced.flush();
+	}}
 />
+{#if browser}
+	<!-- svelte-ignore a11y-autofocus -->
+	<textarea
+		id="textarea"
+		contenteditable="true"
+		bind:value={$data}
+		bind:this={textarea}
+		autofocus
+	/>
+{/if}
+<noscript> JavaScript is required to use this app. </noscript>
 
 <style>
 	#textarea {
@@ -35,5 +40,14 @@
 		outline: none;
 		font-size: 17px;
 		font-family: 'system-ui';
+		resize: none;
+		box-sizing: border-box;
+		border: none;
+	}
+	noscript {
+		margin: 20px;
+		font-size: 17px;
+		font-family: 'system-ui';
+		display: block;
 	}
 </style>
